8b5c9dacb935bea23da6269e86d8675f3a094bd6
Kevin <jefercort@gmail.com>
2025-03-23 17:23:25 +0400

    I made modifications on RabdomFox function and it Show his differentes way to typing thet function

---
+++ components/RabdomFox.tsx
@@ -0,0 +1,24 @@
+// Nosotros usamos JSX.Element para indicar que si o si estamos devolviendo un componente de React
+
+import type { FunctionComponent, FC, JSX } from "react"
+
+// 1. Forma implicita
+// export const RandomFox = () => {
+//     return <Image />
+// }
+const randomNumber = Math.floor(Math.random() * 123) + 1;
+// 2. Forma explicita donde estamos tipando el retorno de la funcion
+export const RandomFox = (): JSX.Element => {
+    const image: string = `https://randomfox.ca/images/${randomNumber}.jpg`
+    return <img width={320} height="auto" src={image} className=" rounded-2xl" />
+}
+
+// 3. Forma explicita usando FunctionComponent tipando directamente la constante
+// export const RandomFox: FunctionComponent = () => {
+//     return <Image />
+// }
+
+// 4. Forma explicita usando FC tipando directamente la constante
+// export const RandomFox: FC = () => {
+//     return <Image />
+// }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { RandomFox } from "../../components/RabdomFox";
+
 export default function Home() {
   return (
     <div>
       <main>
         <h1>
           Hello Kevin
+          <RandomFox />
         </h1>
       </main>
       <footer>
