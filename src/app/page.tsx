fdf10dfdd2b14c2d85fa5f12f105d4aa6b85c5ec
Kevin <jefercort@gmail.com>
2025-03-23 17:50:47 +0400

    I added a .map to use a useState and how to type a use State

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,30 @@
+'use client'
+import { useState } from "react";
 import { RandomFox } from "../../components/RabdomFox";
 
+const randomNumber = () =>  Math.floor(Math.random() * 123) + 1;
+
 export default function Home() {
-  const randomNumber = Math.floor(Math.random() * 123) + 1;
+  const [images, setImages] = useState<string[]>([
+    `https://randomfox.ca/images/${randomNumber()}.jpg`,
+    `https://randomfox.ca/images/${randomNumber()}.jpg`,
+    `https://randomfox.ca/images/${randomNumber()}.jpg`,
+    `https://randomfox.ca/images/${randomNumber()}.jpg`
+  ]);
 
   return (
     <div>
       <main>
         <h1>
           Hello Kevin
-          <RandomFox image={`https://randomfox.ca/images/${randomNumber}.jpg`} />
+          {
+            images.map((image, index) => (
+              <div key={index} className="p-4">
+                <RandomFox image={image} />
+
+              </div>
+            ))
+          }
         </h1>
       </main>
       <footer>
