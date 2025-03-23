6ba321014094db6bcefe3ec9eed6415e683f92a9
Kevin <jefercort@gmail.com>
2025-03-23 18:04:36 +0400

    I modified the function, creating a randomId, and transforming the map by type ImageItems

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,20 @@ import { RandomFox } from "../../components/RabdomFox";
 
 const randomNumber = () =>  Math.floor(Math.random() * 123) + 1;
 
+type ImageItems = {
+  id: number;
+  url: string;
+};
+
+//Generate a random Id for the image in string format
+const randomId = () => Math.floor(Math.random() * 123) + 1;
+
 export default function Home() {
-  const [images, setImages] = useState<string[]>([
-    `https://randomfox.ca/images/${randomNumber()}.jpg`,
-    `https://randomfox.ca/images/${randomNumber()}.jpg`,
-    `https://randomfox.ca/images/${randomNumber()}.jpg`,
-    `https://randomfox.ca/images/${randomNumber()}.jpg`
+  const [images, setImages] = useState<ImageItems[]>([
+    { id: randomId(), url:`https://randomfox.ca/images/${randomNumber()}.jpg`},
+    { id: randomId(), url:`https://randomfox.ca/images/${randomNumber()}.jpg`},
+    { id: randomId(), url:`https://randomfox.ca/images/${randomNumber()}.jpg`},
+    { id: randomId(), url:`https://randomfox.ca/images/${randomNumber()}.jpg`},
   ]);
 
   return (
@@ -18,9 +26,9 @@ export default function Home() {
         <h1>
           Hello Kevin
           {
-            images.map((image, index) => (
-              <div key={index} className="p-4">
-                <RandomFox image={image} />
+            images.map(({ id, url }) => (
+              <div key={id} className="p-4">
+                <RandomFox image={url} />
 
               </div>
             ))
